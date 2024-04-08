6648877b5c75711b6ed7fb578254f64653d13e58
ceddybi <ceddymuhoza@gmail.com>
2024-04-08 13:03:34 -0400

    feat(admin) disputes ui init

--- src/containers/Admin/disputes.req.tsx
+++ src/containers/Admin/disputes.req.tsx
@@ -1,66 +1,122 @@
-"use client";
+'use client';
 
-import { Container } from "@/components/container";
 import {
+  CoreButtonDropdownType,
   CoreText,
   Layout,
-  ProgressBar,
   Sorting,
+  SplitButton,
   Table,
   TableBody,
   TableCell,
   TableHeader,
   TableHeading,
   TableRow,
-} from "@uuixjs/uuixweb";
-import React from "react";
+} from '@uuixjs/uuixweb';
+import React from 'react';
 
-const ReqList = () => (
-  <Table alternateRows>
-    <TableHeader>
-      <TableRow>
-        <TableHeading sorting={Sorting.Default}>Country</TableHeading>
-        <TableHeading>Views</TableHeading>
-        <TableHeading>Percent</TableHeading>
-        <TableHeading>Graph</TableHeading>
-      </TableRow>
-    </TableHeader>
-    <TableBody>
-      <TableRow>
-        <TableCell>United States</TableCell>
-        <TableCell>200</TableCell>
-        <TableCell>26%</TableCell>
-        <TableCell>
-          <ProgressBar value={26} mask />
-        </TableCell>
-      </TableRow>
-      <TableRow>
-        <TableCell>Germany</TableCell>
-        <TableCell>148</TableCell>
-        <TableCell>14%</TableCell>
-        <TableCell>
-          <ProgressBar value={14} mask />
-        </TableCell>
-      </TableRow>
-      <TableRow>
-        <TableCell>Netherlands</TableCell>
-        <TableCell>120</TableCell>
-        <TableCell>10%</TableCell>
-        <TableCell>
-          <ProgressBar value={10} mask />
-        </TableCell>
-      </TableRow>
-      <TableRow>
-        <TableCell>Brazil</TableCell>
-        <TableCell>100</TableCell>
-        <TableCell>8%</TableCell>
-        <TableCell>
-          <ProgressBar value={8} mask />
-        </TableCell>
-      </TableRow>
-    </TableBody>
-  </Table>
-);
+import { Container } from '@/components/container';
+import type { OrderType, UserType } from '@/components/types.generated';
+
+interface ReqListProps {
+  order?: OrderType;
+  owner?: UserType;
+  status?: string;
+  type?: string;
+}
+
+const ReqList = () => {
+  const reqs: ReqListProps[] = [
+    {
+      order: {
+        id: '1',
+        name: 'Order 1',
+        price: 100,
+        status: 'Disputed',
+        type: 'Dispute',
+      },
+      owner: {
+        id: '1',
+        fullname: 'User 1',
+      },
+      status: 'Disputed',
+      type: 'Dispute',
+    },
+    {
+      order: {
+        id: '2',
+        name: 'Order 2',
+        price: 100,
+        status: 'Disputed',
+        type: 'Dispute',
+      },
+      owner: {
+        id: '2',
+        fullname: 'User 2',
+      },
+      status: 'Disputed',
+      type: 'Dispute',
+    },
+    {
+      order: {
+        id: '3',
+        name: 'Order 3',
+        price: 100,
+        status: 'Disputed',
+        type: 'Dispute',
+      },
+      owner: {
+        id: '3',
+        fullname: 'User 3',
+      },
+      status: 'Disputed',
+      type: 'Dispute',
+    },
+    {
+      order: {
+        id: '4',
+        name: 'Order 4',
+        price: 100,
+        status: 'Disputed',
+        type: 'Dispute',
+      },
+      owner: {
+        id: '4',
+        fullname: 'User 4',
+      },
+      status: 'Disputed',
+      type: 'Dispute',
+    },
+  ];
+  return (
+    <Table alternateRows>
+      <TableHeader>
+        <TableRow>
+          <TableHeading sorting={Sorting.Default}>Order</TableHeading>
+          <TableHeading>User</TableHeading>
+          <TableHeading>State</TableHeading>
+          <TableHeading>Action</TableHeading>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {reqs.map((req) => (
+          <TableRow key={req.order?.id}>
+            <TableCell>{req.order?.name}</TableCell>
+            <TableCell>{req.owner?.fullname}</TableCell>
+            <TableCell>{req.status}</TableCell>
+            <TableCell>
+              <SplitButton
+                dropdown={{ type: CoreButtonDropdownType.ArrowDown }}
+              >
+                Action X
+              </SplitButton>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
 
 export const DisputesReq = () => {
   return (
