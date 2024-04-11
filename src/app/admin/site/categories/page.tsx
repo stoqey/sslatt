43b0cd4f5efa10e8c1c0e9a58016727f673a1f74
ceddybi <ceddymuhoza@gmail.com>
2024-04-11 02:57:17 -0400

    chore(meta): category meta

--- src/app/admin/site/categories/page.tsx
+++ src/app/admin/site/categories/page.tsx
@@ -1,4 +1,17 @@
+import type { Metadata } from 'next';
+
 import AdminSiteCategories from '@/containers/Admin/categories';
+import { fetchConfig } from '@/lib/config/server';
+
+export async function generateMetadata(): Promise<Metadata> {
+  // read route params
+  const config = await fetchConfig();
+  const siteName = config?.name;
+
+  return {
+    title: `Admin Category Settings ${siteName ? `- ${siteName}` : ''}`,
+  };
+}
 
 export default function AdminCategories() {
   return <AdminSiteCategories />;
